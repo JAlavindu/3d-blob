bcc3e0ba4e996b45ed255ab1d609818d48c0996f
lavindu perera <lavi156perera@gmail.com>
2025-12-06 20:49:26 +0530

    created basic blob

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Environment } from "@react-three/drei";
+import Blob from "./Blob";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="canvas-container">
+      <Canvas camera={{ position: [0, 0, 3], fov: 45 }}>
+        <Suspense fallback={null}>
+          <color attach="background" args={["#000000"]} />
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[10, 10, 5]} intensity={1} />
+          <Blob />
+          <OrbitControls enableZoom={true} />
+          <Environment preset="city" />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
 }
 
-export default App
+export default App;
